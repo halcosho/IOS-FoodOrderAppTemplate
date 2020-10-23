3ceee232408ddfd69a2a769918518cd1d4480e0e
Nejra <nejra_lacevic@hotmail.com>
2020-10-23 22:56:28 +0200

    Initial Commit

---
+++ FoodOrderTemplateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrderTemplateApp
+//
+//  Created by Halil on 23.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
